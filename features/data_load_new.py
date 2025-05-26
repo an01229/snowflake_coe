d75675e8da9754d95f3a1e3d29d4d8e93d3a0b64
Sai Krishna Cherukupally <saikrishna.c@anblicks.com>
2025-05-26 13:15:13 -0500

    updted code to load entire data into sql server table

--- features/data_load_new.py
+++ features/data_load_new.py
@@ -2,6 +2,7 @@ from sqlalchemy import create_engine
 import urllib
 import pandas as pd
 
+# Connection string
 params = urllib.parse.quote_plus(
     "DRIVER={ODBC Driver 18 for SQL Server};"
     "SERVER=sf-coe-sql-server.database.windows.net;"
@@ -14,5 +15,16 @@ params = urllib.parse.quote_plus(
 
 engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
+# Load entire CSV
 df = pd.read_csv("/Users/saikrishnareddy/onlinefraud.csv")
-df.head(10000).to_sql("online_payment_fraud_detection", con=engine, if_exists='replace', index=False, chunksize=1000)
+
+#Ingesting the data in chunks into SQL Server object
+df.to_sql(
+    name="online_payment_fraud_detection",
+    con=engine,
+    if_exists="replace",
+    index=False,
+    chunksize=5000
+)
+
+print(f"Successfully inserted {len(df)} records.")
