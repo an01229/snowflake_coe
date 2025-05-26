6ca0fe1846d3a3d6d1e5f35c7adee40da6d3ecde
Sai Krishna Cherukupally <saikrishna.c@anblicks.com>
2025-05-26 13:29:54 -0500

    updated code to add try catch block

--- features/data_load_new.py
+++ features/data_load_new.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 import urllib
 import pandas as pd
 
-# Connection string
 params = urllib.parse.quote_plus(
     "DRIVER={ODBC Driver 18 for SQL Server};"
     "SERVER=sf-coe-sql-server.database.windows.net;"
@@ -15,16 +14,29 @@ params = urllib.parse.quote_plus(
 
 engine = create_engine("mssql+pyodbc:///?odbc_connect=%s" % params)
 
-# Load entire CSV
-df = pd.read_csv("/Users/saikrishnareddy/onlinefraud.csv")
+try:
+    df = pd.read_csv("/Users/saikrishnareddy/onlinefraud.csv")
 
-#Ingesting the data in chunks into SQL Server object
-df.to_sql(
-    name="online_payment_fraud_detection",
-    con=engine,
-    if_exists="replace",
-    index=False,
-    chunksize=5000
-)
+    print(f"Attempting to load {len(df)} records...")
 
-print(f"Successfully inserted {len(df)} records.")
+    print(df.dtypes)
+    print(df.head())
+
+    # Convert columns to appropriate types
+    df["amount"] = pd.to_numeric(df["amount"], errors="coerce")
+    df["step"] = pd.to_numeric(df["step"], errors="coerce")
+
+
+    df.to_sql(
+        name="online_payment_fraud_detection",
+        con=engine,
+        if_exists="append",  # or "append"
+        index=False,
+        chunksize=2000
+    )
+
+    print("Data load successful.")
+
+except Exception as e:
+    print("❌ Error occurred during load:")
+    print(e)
